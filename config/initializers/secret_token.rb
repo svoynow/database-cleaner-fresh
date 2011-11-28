a31d5ffb2f6579e8e13bc5746f1088430ff15eb7
Vounow <avounow@wm720l44010a.(none)>
2011-11-28 09:25:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fresh::Application.config.secret_token = '3e8875e104593b6630e7d3db7523dac46f62f8c8770504a464e4ef31e2bb11783e120c7e9d603b48a77b5d109b6752147e94c134e49e1f4104c1a85b062fd5cb'
